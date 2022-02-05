96f1a231f8a6fc487adfb35690f55c7035ef2921
Heber Magno <heber2014gbi@gmail.com>
2022-02-05 11:56:47 -0300

    concluido galeria de imagens

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
+
+import styles from "./style.module.css";
+
 import Header from "../Header/Header";
 import ItemImage from "../ItemImage/ItemImage";
 
 function Home () {
 
   const [totalItems, setTotalitems] = useState(2)
+  const [showPopup, setShowpopup] = useState(false)
 
   const images = ["image-product-1","image-product-2","image-product-3","image-product-4"];
 
   return(
-    <div>
+    <div className={styles.Page}>
+      {
+        showPopup &&       
+        <div className={styles.PopupImage}>
+          <button className={styles.Close} onClick={() => setShowpopup(!showPopup)}>
+            <img src="/images/icon-close.svg"/>
+          </button>
+          <ItemImage images={images} popup={true}/>
+        </div>
+      }
       <Header 
         avatar={"image-avatar.png"}
         itemsTotal={totalItems}
       />
-      <ItemImage images={images}/>
+
+      <div className={styles.Content}>
+        <ItemImage images={images} onClick={() => setShowpopup(!showPopup)}/>
+        <div className={styles.info}></div>
+      </div>
+      
+
       
     </div>
   )
--- src/components/ItemImage/ItemImage.jsx
+++ src/components/ItemImage/ItemImage.jsx
@@ -3,24 +3,49 @@ import classNames from "classnames";
 
 import styles from "./styles.module.css";
 
-function ItemImage (images) {
+function ItemImage(props) {
 
   const [atualImage, setAtualimage] = useState(0);
 
-  const alterImage = (pos) => setAtualimage(pos);
+  const alterImage = (pos) => {
+    setAtualimage(pos);
+  }
 
-  const isSelected = (pos) => pos === atualImage; 
+  const alteraBtn = (btn) => {
+    if (btn === "next") {
+      atualImage + 1 > props.images.length - 1 ? setAtualimage(0) : setAtualimage(atualImage + 1);
+    }
+    else {
+      atualImage - 1 < 0 ? setAtualimage(props.images.length - 1) : setAtualimage(atualImage - 1);
+    }
+  }
 
-console.log(images.images);
-  return(
+  const isSelected = (pos) => pos === atualImage;
+
+  return (
     <div className={styles.ProductGallery}>
-      <img className={styles.ImageProduct} 
-           src={`/images/${images.images[atualImage]}.jpg`} 
-           width="100%"
-      />
+      <button 
+        className={styles.BtnPopup}
+        onClick={props.onClick} 
+        disabled={props.popup}>
+          <img className={styles.ImageProduct}
+            src={`/images/${props.images[atualImage]}.jpg`}
+            width="100%"
+          />
+      </button>
+      { props.popup && <>
+            <button className={styles.prev} onClick={() => alteraBtn("prev")}>
+              <img src={`/images/icon-previous.svg`} />
+            </button>
+            <button className={styles.next} onClick={() => alteraBtn("next")}>
+              <img src={`/images/icon-next.svg`} />
+            </button>
+          </>
+      }
+
       <div className={styles.Thumbnail}>
         {
-          images.images.map((value, pos) => 
+          props.images.map((value, pos) =>
             <button
               className={classNames(styles.Button, {
                 [styles.selectedBtn]: isSelected(pos)
@@ -41,6 +66,10 @@ console.log(images.images);
       </div>
     </div>
   )
+}
+
+ItemImage.defaultProps = {
+  popup: false
 }
 
 export default ItemImage;
